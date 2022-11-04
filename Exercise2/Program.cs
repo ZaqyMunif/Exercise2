e21e1a6b75998ac7029e7b43a349154cddb779f4
ZaqyMunif <zaqymunif17@gmail.com>
2022-11-04 16:07:12 +0700

    variable input

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -8,6 +8,22 @@ namespace Exercise2
 {
     class Program
     {
+        private int[] zaqy = new int[117];
+        private int n;
+        
+        public void Input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number element in array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 117))
+                    break;
+                else
+                    Console.WriteLine("\n Maksimal Array 117 element. \n");
+            }
 
+        }
     }
 }
